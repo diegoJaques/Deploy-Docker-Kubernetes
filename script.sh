20dffa93de1d77d98a3b88533f269532b9457087
diegojaques <diegojaques2016@gmail.com>
2023-01-03 23:12:00 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "criando as imagens.............."
+
+docker build -t diegojaques/projeto-backend:1.0 backend/.
+docker build -t diegojaques/projeto-bancodedados:1.0 database/.
+
+echo "realizando o push das imagens"
+docker push diegojaques/projeto-backend:1.0
+docker push diegojaques/projeto-bancodedados:1.0
+
+
+echo "criando os serviços no Cluster kubernets"
+
+kubectl apply -f ./services.yml
+
+echo "criando os deplyments"
+
+kubectl apply -f ./deployments.yml
+
+
